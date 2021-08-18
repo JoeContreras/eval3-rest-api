5fdcc2652506f6ccbda6f9e3f7bc2403fed23a2d
JoeContreras <joe.contreras888@gmail.com>
2021-08-17 23:21:33 -0500

    Leer un solo vendedor

--- class/employees.php
+++ class/employees.php
@@ -77,11 +77,14 @@ class Employee{
     public function getSingleEmployee(){
         $sqlQuery = "SELECT
                         id, 
-                        name, 
-                        email, 
-                        age, 
-                        designation, 
-                        created
+                        lote, 
+                        nombre, 
+                        apellido, 
+                        inicio, 
+                        terminacion, 
+                        tipo, 
+                        numPieza, 
+                        defPieza
                       FROM
                         ". $this->db_table ."
                     WHERE 
@@ -96,11 +99,14 @@ class Employee{
 
         $dataRow = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        $this->name = $dataRow['name'];
-        $this->email = $dataRow['email'];
-        $this->age = $dataRow['age'];
-        $this->designation = $dataRow['designation'];
-        $this->created = $dataRow['created'];
+        $this->lote = $dataRow['lote'];
+        $this->nombre = $dataRow['nombre'];
+        $this->apellido = $dataRow['apellido'];
+        $this->inicio = $dataRow['inicio'];
+        $this->terminacion = $dataRow['terminacion'];
+        $this->tipo = $dataRow['tipo'];
+        $this->numPieza = $dataRow['numPieza'];
+        $this->defPieza = $dataRow['defPieza'];
     }
 
     // UPDATE
@@ -108,29 +114,38 @@ class Employee{
         $sqlQuery = "UPDATE
                         ". $this->db_table ."
                     SET
-                        name = :name, 
-                        email = :email, 
-                        age = :age, 
-                        designation = :designation, 
-                        created = :created
+                        lote = :lote, 
+                        nombre = :nombre, 
+                        apellido = :apellido, 
+                        inicio = :inicio, 
+                        terminacion = :terminacion, 
+                        tipo = :tipo, 
+                        numPieza = :numPieza, 
+                        defPieza = :defPieza
                     WHERE 
                         id = :id";
 
         $stmt = $this->conn->prepare($sqlQuery);
 
-        $this->name=htmlspecialchars(strip_tags($this->name));
-        $this->email=htmlspecialchars(strip_tags($this->email));
-        $this->age=htmlspecialchars(strip_tags($this->age));
-        $this->designation=htmlspecialchars(strip_tags($this->designation));
-        $this->created=htmlspecialchars(strip_tags($this->created));
+        $this->lote=htmlspecialchars(strip_tags($this->lote));
+        $this->nombre=htmlspecialchars(strip_tags($this->nombre));
+        $this->apellido=htmlspecialchars(strip_tags($this->apellido));
+        $this->inicio=htmlspecialchars(strip_tags($this->inicio));
+        $this->terminacion=htmlspecialchars(strip_tags($this->terminacion));
+        $this->tipo=htmlspecialchars(strip_tags($this->tipo));
+        $this->numPieza=htmlspecialchars(strip_tags($this->numPieza));
+        $this->defPieza=htmlspecialchars(strip_tags($this->defPieza));
         $this->id=htmlspecialchars(strip_tags($this->id));
 
         // bind data
-        $stmt->bindParam(":name", $this->name);
-        $stmt->bindParam(":email", $this->email);
-        $stmt->bindParam(":age", $this->age);
-        $stmt->bindParam(":designation", $this->designation);
-        $stmt->bindParam(":created", $this->created);
+        $stmt->bindParam(":lote", $this->lote);
+        $stmt->bindParam(":nombre", $this->nombre);
+        $stmt->bindParam(":apellido", $this->apellido);
+        $stmt->bindParam(":inicio", $this->inicio);
+        $stmt->bindParam(":terminacion", $this->terminacion);
+        $stmt->bindParam(":tipo", $this->tipo);
+        $stmt->bindParam(":numPieza", $this->numPieza);
+        $stmt->bindParam(":defPieza", $this->defPieza);
         $stmt->bindParam(":id", $this->id);
 
         if($stmt->execute()){
